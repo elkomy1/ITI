238ef70419b08510a159fe1222210e80fd7bce98
komy <youssefhossam939@gmail.com>
2023-11-23 09:19:55 +0200

    my first bashscript

---
+++ linux/task1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir ~/mydirectory
+mkdir ~/mydirectory/seconddirectory
+touch ~/mydirectory/seconddirectory/mynotepaper
+mv ~/mydirectory/mynotepaper ~/mydirectory/mynewpaper
+echo "this is script"
